916d9d5255c45e9a0d2034eb35143ed884dcd7e3
David Marsh <40401344+Daveym1@users.noreply.github.com>
2023-01-31 14:04:04 +0000

    pushed manager answers to teamMember array

--- index.js
+++ index.js
@@ -11,101 +11,92 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 // Adding prompts for manager
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "Please enter the team manager's name",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Please enter the team manager's Employee ID",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Please enter the team manager's email address",
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "Please enter the team manager's office number",
-  },
-  {
-    type: "list",
-    name: "newTeamMember",
-    message: "Do you want to add another team member?",
-    choices: ["Add an engineer", "Add an intern", "Finish building team"],
-  },
 
-  {
-    type: "input",
-    name: "engineerName",
-    message: "Name?",
-  },
-  {
-    type: "input",
-    name: "engineerId",
-    message: "ID?",
-  },
-  {
-    type: "input",
-    name: "engineerEmail",
-    message: "Email address?",
-  },
-  {
-    type: "input",
-    name: "engineerGithub",
-    message: "GitHub username?",
-  },
+let teamMembers = [];
 
-  {
-    type: "list",
-    name: "addMoreTeamMembers",
-    message: "Do you want to add another team member?",
-    choices: ["Add an engineer", "Add an intern", "Finish building team"],
-  },
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter the team manager's name",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter the team manager's Employee ID",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter the team manager's email address",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Please enter the team manager's office number",
+    },
+  ])
 
-  {
-    type: "input",
-    name: "engineerName",
-    message: "What is the engineer's name?",
-  },
-  {
-    type: "input",
-    name: "engineerId",
-    message: "What is the engineer's ID?",
-  },
-  {
-    type: "input",
-    name: "engineerEmail",
-    message: "What is the engineer's email?",
-  },
-  {
-    type: "input",
-    name: "engineerGithub",
-    message: "What is the engineer's GitHub username?",
-  },
+  // collecting Manager info and pushing to teamMembers array
 
-  {
-    type: "input",
-    name: "internName",
-    message: "What is the intern's name?",
-  },
-  {
-    type: "input",
-    name: "internId",
-    message: "What is the intern's ID?",
-  },
-  {
-    type: "input",
-    name: "internEmail",
-    message: "What is the intern's email?",
-  },
-  {
-    type: "input",
-    name: "internSchool",
-    message: "What is the intern's school?",
-  },
-]);
+  .then((answers) => {
+    const manager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.officeNumber
+    );
+    teamMembers.push(manager);
+    console.log(teamMembers);
+  });
+
+// Adding prompts for engineers
+
+// inquirer.prompt([
+//   {
+//     type: "input",
+//     name: "engineerName",
+//     message: "Name?",
+//   },
+//   {
+//     type: "input",
+//     name: "engineerId",
+//     message: "ID?",
+//   },
+//   {
+//     type: "input",
+//     name: "engineerEmail",
+//     message: "Email address?",
+//   },
+//   {
+//     type: "input",
+//     name: "engineerGithub",
+//     message: "GitHub username?",
+//   },
+// ]);
+
+// // Adding prompts for interns
+
+// inquirer.prompt([
+//   {
+//     type: "input",
+//     name: "internName",
+//     message: "What is the intern's name?",
+//   },
+//   {
+//     type: "input",
+//     name: "internId",
+//     message: "What is the intern's ID?",
+//   },
+//   {
+//     type: "input",
+//     name: "internEmail",
+//     message: "What is the intern's email?",
+//   },
+//   {
+//     type: "input",
+//     name: "internSchool",
+//     message: "What is the intern's school?",
+//   },
+// ]);
